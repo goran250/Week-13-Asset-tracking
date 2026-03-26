f209224961813be0f865d837c51ac5777a1a8976
goran131 <goran.rosenberg@tomelilla.nu>
2026-03-26 10:54:30 +0100

    GR

--- MobilePhone.cs
+++ MobilePhone.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 namespace Asset_tracking
 {
-    public class MobilePhone; : Asset
+    public class MobilePhone : Asset
     {
 	    public MobilePhone(string brand, string modelName, DateTime purchaseDate, int price, string location)
 	    {
